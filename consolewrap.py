c03e2ed9ef6e71a36adbcbfaece600311fb56c82
unknownuser88 <davidbekoyan@gmail.com>
2014-03-04 23:23:48 -0800

    22

--- consolewrap.py
+++ consolewrap.py
@@ -13,29 +13,31 @@ class ConsolewrapCommand(sublime_plugin.TextCommand):
                 sublime.status_message('Please Make A Selection');
             else:
                 var_text = view.substr(cursor)
-                view.insert(edit, line_region.end(), "\n%sconsole.log('%s ' , %s);" % (match.group(1), var_text, var_text))
+                inserted_console_template = "\n%sconsole.log('%s ' , %s);"
+                if 'source.coffee' in view.scope_name(0):
+                  inserted_console_template = "\n%sconsole.log '%s', %s"
+                view.insert(edit, line_region.end(), inserted_console_template % (match.group(1), var_text, var_text))
                 end = view.line(line_region.end() + 1).end()
                 view.sel().clear()
                 view.sel().add(sublime.Region(end, end))
 
 class ConsoleremoveCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.get_selections()
-        cursor = self.view.sel()[0]
-        line_region = self.view.line(cursor)
-        string = self.view.substr(line_region)
-        newstring = re.sub(r"(?m)^((?!//|/\*).)*console\.log.*", '', string)
-        self.view.replace(edit, line_region, newstring)
-        sublime.status_message('removed');
-        self.view.sel().clear()
+        self.get_selections();
+        cursor = self.view.sel()[0];
+        line_region = self.view.line(cursor);
+        string = self.view.substr(line_region);
+        newstring = re.sub(r"(?m)^((?!//|/\*).)*console\.log.*", '', string);
+        self.view.replace(edit, line_region, newstring);
+        self.view.sel().clear();
 
     def get_selections(self):
-        selections = self.view.sel()
-        has_selections = False
+        selections = self.view.sel();
+        has_selections = False;
         for sel in selections:
             if sel.empty() == False:
-                has_selections = True
+                has_selections = True;
         if not has_selections:
-            full_region = sublime.Region(0, self.view.size())
-            selections.add(full_region)
+            full_region = sublime.Region(0, self.view.size());
+            selections.add(full_region);
         return selections
