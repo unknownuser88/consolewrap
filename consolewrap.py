1a086a7904823241533fc2d091dc532d073862c2
Luan Nico <luan.nico@dextra-sw.com>
2014-05-22 08:44:36 -0300

    Made it so that you can add multiple language support easily, added to ruby

--- consolewrap.py
+++ consolewrap.py
@@ -8,6 +8,13 @@ class ConsolewrapCommand(sublime_plugin.TextCommand):
         line_region = view.line(cursor)
         string = view.substr(line_region)
         match = re.search(r"(\s*)", string)
+
+        file_name = view.file_name()
+        if file_name is None:
+            extension = None
+        else:
+            extension = file_name[file_name.rfind('.') + 1:]
+
         if match:
             if cursor.empty():
                 var_text = sublime.get_clipboard()
@@ -15,14 +22,24 @@ class ConsolewrapCommand(sublime_plugin.TextCommand):
                 var_text = view.substr(cursor)
             if var_text[-1:] == ";":
                 var_text = var_text[:-1]
-            if var_text.empty():
+
+            if len(var_text) == 0:
                 sublime.status_message('Please make a selection or copy something.')
             else:
                 var_text_escaped = var_text.replace("'", "\\'")
-                view.insert(edit, line_region.end(), "\n%sconsole.log('%s ' , %s);" % (match.group(1), var_text_escaped, var_text))
+                text = ConsolewrapCommand.get_wrapper(extension, match.group(1), var_text, var_text_escaped)
+                view.insert(edit, line_region.end(), text)
                 end = view.line(line_region.end() + 1).end()
                 view.sel().clear()
                 view.sel().add(sublime.Region(end, end))
+
+    @staticmethod
+    def get_wrapper(lang, spaces, var_text, var_text_escaped):
+        if lang == 'rb' or lang == 'erb':
+            return "\n%sputs '-----------------------------[log][auto][%s]:';p %s" % (spaces, var_text_escaped, var_text)
+        else:
+            return "\n%sconsole.log('%s ' , %s);" % (spaces, var_text_escaped, var_text)
+
 
 class ConsoleremoveCommand(sublime_plugin.TextCommand):
     def run(self, edit):
