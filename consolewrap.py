1cb95abd2b9bd984c987279f7533c924dce7e895
David <davidbekoyan@gmail.com>
2016-08-09 19:05:28 +0400

    add multiple selection support

--- consolewrap.py
+++ consolewrap.py
@@ -4,43 +4,49 @@ import sublime_plugin
 class ConsolewrapCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
-        cursor = view.sel()[0]
-        line_region = view.line(cursor)
-        string = view.substr(line_region)
-        match = re.search(r"(\s*)", string)
+        cursors = view.sel()
+        
+        for cursor in cursors:
+            line_region = view.line(cursor)
+            string = view.substr(line_region)
+            match = re.search(r"(\s*)", string)
 
-        file_name = view.file_name()
-        if file_name is None:
-            extension = None
-        else:
-            extension = file_name[file_name.rfind('.') + 1:]
-
-        if match:
-            if cursor.empty():
-                var_text = sublime.get_clipboard()
+            file_name = view.file_name()
+            if file_name is None:
+                extension = None
             else:
-                var_text = view.substr(cursor)
-            if var_text[-1:] == ";":
-                var_text = var_text[:-1]
+                extension = file_name[file_name.rfind('.') + 1:]
 
-            if len(var_text) == 0:
-                sublime.status_message('Please make a selection or copy something.')
-            else:
-                var_text_escaped = var_text.replace("'", "\\'")
-                text = ConsolewrapCommand.get_wrapper(extension, match.group(1), var_text, var_text_escaped)
-                view.insert(edit, line_region.end(), text)
-                end = view.line(line_region.end() + 1).end()
-                view.sel().clear()
-                view.sel().add(sublime.Region(end, end))
+            if match:
+                if cursor.empty():
+                    var_text = sublime.get_clipboard()
+                else:
+                    var_text = view.substr(cursor)
+                if var_text[-1:] == ";":
+                    var_text = var_text[:-1]
+
+                if len(var_text) == 0:
+                    sublime.status_message('Please make a selection or copy something.')
+                else:
+                    var_text_escaped = var_text.replace("'", "\\'")
+                    text = ConsolewrapCommand.get_wrapper(self, extension, match.group(1), var_text, var_text_escaped)
+                    view.insert(edit, line_region.end(), text)
+                    end = view.line(line_region.end() + 1).end()
+
+        view.sel().clear()
+        view.sel().add(sublime.Region(end, end))
 
     @staticmethod
-    def get_wrapper(lang, spaces, var_text, var_text_escaped):
+    def get_wrapper(self, lang, spaces, var_text, var_text_escaped):
         if lang == 'rb':
             return "\n%sputs '-----------------------------[log][auto][%s]:';p %s" % (spaces, var_text_escaped, var_text)
         elif lang == 'erb':
             return "\n%s<%% puts '-----------------------------[log][auto][%s]:';p %s %%>" % (spaces, var_text_escaped, var_text)
         else:
-            return "\n%sconsole.log('%s', %s);" % (spaces, var_text_escaped, var_text)
+            inserted_console_template = "\n%sconsole.log('%s', %s);" % (spaces, var_text_escaped, var_text)
+            if 'source.coffee' in self.view.scope_name(0):
+                inserted_console_template = "\n%sconsole.log '%s', %s" % (spaces, var_text_escaped, var_text)
+            return inserted_console_template
 
 
 class ConsoleremoveCommand(sublime_plugin.TextCommand):
